47cb5f44866016af583da772bbc5968f7a83606b
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-04-12 18:50:54 +0530

    added authMiddleware, movieMiddleware and movieRoutes

---
+++ src/middlewares/movie.middlewares.js
@@ -0,0 +1,58 @@
+const badRequestResponse = {
+    success: false,
+    err: "",
+    data: {},
+    message: 'Malformed Request | Bad Request'
+};
+
+const { STATUS } = require('../utils/constants');
+
+/**
+ * @param req -> HTTP request object
+ * @param {*} res -> HTTP response object
+ * @param {*} next -> next middleware function
+ * @returns -> whether the request is valid or not
+ */
+
+const validateMovieCreateRequest = async (req, res, next) => {
+    // validate the movie name
+    if (!req.body.name) {
+        badRequestResponse.err = 'The name of the movie is not present in the request';
+        return res.status(STATUS.BAD_REQUEST).json(badRequestResponse);
+    }
+
+    // validate the movie descritpion
+    if (!req.body.description) {
+        badRequestResponse.err = 'The description of the movie is not presnet in the request';
+        return res.status(STATUS.BAD_REQUEST).json(badRequestResponse);
+    }
+
+    // validate the movie casts
+    if (!req.body.casts || !(req.body.casts instanceof Array) || req.body.casts.length <= 0) {
+        badRequestResponse.err = 'The casts of the movie is not present in the request';
+        return res.status(STATUS.BAD_REQUEST).json(badRequestResponse);
+    }
+
+    // validate the movie trailer url
+    if (!req.body.trailerUrl) {
+        badRequestResponse.err = 'The trailerUrl of the movie is not present in the request';
+        return res.status(STATUS.BAD_REQUEST).json(badRequestResponse);
+    }
+
+    // validate the release data of the movie
+    if (!req.body.releaseData) {
+        badRequestResponse.err = 'The releaseDate of the movie is not present in the request';
+        return res.status(STATUS.BAD_REQUEST).json(badRequestResponse);
+    }
+
+    // validate director of the movie 
+    if (!req.body.director) {
+        badRequestResponse.err = 'The director of the movie is not present in the request';
+        return res.status(STATUS.BAD_REQUEST).json(badRequestResponse);
+    }
+    next();
+}
+
+module.exports = {
+    validateMovieCreateRequest
+}
--- src/routes/movie.routes.js
+++ src/routes/movie.routes.js
@@ -1,10 +1,54 @@
 const movieController = require('../controllers/movie.controller');
+const movieMiddleware = require('../middlewares/movie.middlewares');
+const authMiddleware = require('../middlewares/auth.middlewares');
 
 const routes = (app) => {
+    // routes function takes express app object as parameter
+
+    // CREATE
     app.post(
         '/api/v1/movies',
+        authMiddleware.isAuthenticated,
+        authMiddleware.isAdminOrClient,
+        movieMiddleware.validateMovieCreateRequest,
         movieController.createMovie
     );
+
+    // DELETE
+    app.delete(
+        '/api/v1/movies/:id',
+        authMiddleware.isAuthenticated,
+        authMiddleware.isAdminOrClient,
+        movieController.deleteMovie,
+    )
+
+    // READ
+    app.get(
+        '/api/v1/movies/:id',
+        movieController.getMovie
+    )
+
+    // READ
+    app.put(
+        '/api/v1/movies/:id',
+        authMiddleware.isAuthenticated,
+        authMiddleware.isAdminOrClient,
+        movieController.updateMovie,
+    )
+
+    // UPDATE
+    app.patch(
+        '/api/v1/movies/:id',
+        authMiddleware.isAuthenticated,
+        authMiddleware.isAdminOrClient,
+        movieController.updateMovie
+    )
+
+    // UPDATE
+    app.get(
+        '/api/v1/movies/:id',
+        movieController.getMovies
+    )
 }
 
 module.exports = routes;
