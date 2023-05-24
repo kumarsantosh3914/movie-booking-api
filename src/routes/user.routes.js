6c9e630ae55631bec583f408ae67f4d6f2575f5d
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-05-24 23:01:39 +0530

    done user routes with validation

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,13 @@
 const userController = require('../controllers/user.controller');
+const userMiddleware = require('../middlewares/user.middlewares');
+const authMiddleware = require('../middlewares/auth.middlewares');
 
 const route = (app) => {
     app.patch(
         '/api/v1/user/:id',
+        authMiddleware.isAuthenticated,
+        userMiddleware.validateUpdateUserRequest,
+        authMiddleware.isAdmin,
         userController.update,
     )
 }
