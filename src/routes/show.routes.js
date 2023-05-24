f5cd2ca2f6489af16548c9fc0c7e0f3ab5f7b42a
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-05-24 23:29:51 +0530

    added show routes with authentication

---
+++ src/routes/show.routes.js
@@ -0,0 +1,35 @@
+const showController = require('../controllers/show.controller');
+const authMiddlewares = require('../middlewares/auth.middlewares');
+const showMiddlewares = require('../middlewares/show.middlewares');
+
+const routes = (app) => {
+    app.post(
+        'api/v1/shows',
+        authMiddlewares.isAuthenticated,
+        authMiddlewares.isAdminOrClient,
+        showMiddlewares.validationCreateShowRequest,
+        showController.create
+    );
+
+    app.get(
+        'api/v1/shows',
+        showController.getShow,
+    );
+
+    app.delete(
+        'api/v1/shows/:id',
+        authMiddlewares.isAuthenticated,
+        authMiddlewares.isAdminOrClient,
+        showController.destroy
+    );
+
+    app.patch(
+        'api/v1/shows/:id',
+        authMiddlewares.isAuthenticated,
+        authMiddlewares.isAdminOrClient,
+        showMiddlewares.validateShowUpdateRequest,
+        showController.update
+    );
+}
+
+module.exports = routes;
