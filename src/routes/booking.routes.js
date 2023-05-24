5afa5812aebeec7f6a634a93c4fb16c017bf5e42
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-05-24 23:13:06 +0530

    done booking routes with authentication

--- src/routes/booking.routes.js
+++ src/routes/booking.routes.js
@@ -1,10 +1,41 @@
 const bookingController = require('../controllers/booking.controller');
 
+const authMiddleware = require('../middlewares/auth.middlewares');
+const bookingMiddleware = require('../middlewares/booking.middlewares');
+
 const routes = (app) => {
     app.post(
         '/api/v1/bookings',
+        authMiddleware.isAuthenticated,
+        bookingMiddleware.validateBookingCreateRequest,
         bookingController.create
-    )
+    );
+
+    app.patch(
+        'api/v1/bookings/:id',
+        authMiddleware.isAuthenticated,
+        bookingMiddleware.canChangeStatus,
+        bookingController.update
+    );
+
+    app.get(
+        'api/v1/bookings',
+        authMiddleware.isAuthenticated,
+        bookingController.getBookings
+    );
+
+    app.get(
+        'api/v1/bookings/all',
+        authMiddleware.isAuthenticated,
+        authMiddleware.isAdmin,
+        bookingController.getAllBookings,
+    );
+
+    app.get(
+        'api/v1/bookings/:id',
+        authMiddleware.isAuthenticated,
+        bookingController.getBookingById,
+    );
 }
 
 module.exports = routes;
