175d4b6e8d46135788c69c12d10e52fbbe6912d6
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-05-24 23:21:22 +0530

    added payments routes

---
+++ src/routes/payment.routes.js
@@ -0,0 +1,26 @@
+const paymentController = require('../controllers/payment.controller');
+const authMiddlewares = require('../middlewares/auth.middlewares');
+const paymentMiddlewares = require('../middlewares/payment.middlewares');
+
+const routes = (app) => {
+    app.post(
+        'api/v1/payments',
+        authMiddlewares.isAuthenticated,
+        paymentMiddlewares.verifyPaymentCreateRequest,
+        paymentController.create
+    );
+
+    app.get(
+        'api/v1/payments/:id',
+        authMiddlewares.isAuthenticated,
+        paymentController.getPaymentDetailsById
+    );
+
+    app.get(
+        'api/v1/payments',
+        authMiddlewares.isAuthenticated,
+        paymentController.getAllPayments,
+    );
+}
+
+module.exports = routes;
